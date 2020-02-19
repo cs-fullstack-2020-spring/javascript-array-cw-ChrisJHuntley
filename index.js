ef14393e8d4302d05cbb62e722828bcaa06d69e3
autumn-ragland <raglanda998@gmail.com>
2020-02-19 16:00:58 -0600

    graded

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // let Names=['John','Paul', 'Gorge','Pete']
 // console.log(Names[3])
+// // !! : keep in mind that this only works bc Pete is at the end
+// // !! : you can change the value of an array item by index
 // let change=Names.splice(3,1)
 // Names.push('Ringo')
 // Names.push('Yoko')
@@ -11,19 +13,22 @@
 // let age3=prompt('What is your Age?')
 // let array=[age, age1, age2, age3];
 // console.log(array)
+// // !! : you don't have to assign array.splice to a variable, you're updating the exisiting variable
 // let remove=array.splice(2,1)
 // let newer=array.splice
 // console.log(array)
 
-let name1=prompt('Name?')
-let name2=prompt('Name?')
-let name3=prompt('Name?')
-let name4=prompt('Name?')
+// // !! : use better prompt messages
+// let name1=prompt('Name?')
+// let name2=prompt('Name?')
+// let name3=prompt('Name?')
+// let name4=prompt('Name?')
 
-array=[name1, name2,name3,name4]
-if(name1,name2,name3, name4='Kenn'){
-    console.log('')
-}
-console.log(array)
+// array=[name1, name2,name3,name4]
+// // !! : if you want to test mutliple conditions they need to be seperated by AND or OR or in seperate an else if pattern
+// if(name1,name2,name3, name4='Kenn'){
+//     console.log('')
+// }
+// console.log(array)
 
 
