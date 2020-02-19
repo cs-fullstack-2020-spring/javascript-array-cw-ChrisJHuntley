77b39f1d58de267bdd41928d15f2762addb7b112
chris huntley <cjhuntley4@gmail.com>
2020-02-19 15:18:52 -0600

    NEED MORE PRACTICE

---
+++ index.js
@@ -0,0 +1,29 @@
+// let Names=['John','Paul', 'Gorge','Pete']
+// console.log(Names[3])
+// let change=Names.splice(3,1)
+// Names.push('Ringo')
+// Names.push('Yoko')
+// console.log(Names)
+
+// let age=prompt('What is your Age?')
+// let age1=prompt('What is your Age?')
+// let age2=prompt('What is your Age?')
+// let age3=prompt('What is your Age?')
+// let array=[age, age1, age2, age3];
+// console.log(array)
+// let remove=array.splice(2,1)
+// let newer=array.splice
+// console.log(array)
+
+let name1=prompt('Name?')
+let name2=prompt('Name?')
+let name3=prompt('Name?')
+let name4=prompt('Name?')
+
+array=[name1, name2,name3,name4]
+if(name1,name2,name3, name4='Kenn'){
+    console.log('')
+}
+console.log(array)
+
+
